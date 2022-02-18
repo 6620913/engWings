b5a18cc7f4ca9676a4f80b835e0015a16828c915
NEERAJ <6620913@gmail.com>
2022-02-18 20:12:12 +0530

    adding static files to projects

--- engWings/wings/views.py
+++ engWings/wings/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+
+
+    #dests=courses.objects.all()
+    #return render(request,'index.html',{'dests':dests})
+    return render(request,'index.html')
+
