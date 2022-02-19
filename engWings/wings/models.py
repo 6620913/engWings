65e326297b14fe8ff7971dff8cc31b530c844e26
NEERAJ <6620913@gmail.com>
2022-02-19 22:24:36 +0530

    scrollbar modification

--- engWings/wings/models.py
+++ engWings/wings/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Videos(models.Model):
+    thumbnail = models.ImageField(upload_to="pics")
     title = models.CharField(max_length=200)
     video =models.FileField(upload_to="wings/%y")
     udate =models.CharField(max_length=100)
