7bf667922669655a25203e7e70c469ba4dab11c9
NEERAJ <6620913@gmail.com>
2022-02-19 13:41:15 +0530

    basic template

--- engWings/wings/urls.py
+++ engWings/wings/urls.py
@@ -6,5 +6,9 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name='index'),
-    path('index/',views.index, name='index'),
+    path('index',views.index, name='index'),
+    path('about',views.about, name='about'),
+    path('content',views.content, name='content'),
+    path('contact',views.contact, name='contact'),
+
 ]
--- engWings/wings/views.py
+++ engWings/wings/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
 
+    #dests=courses.objects.all()
+    #return render(request,'index.html',{'dests':dests})
+
 # Create your views here.
 def index(request):
 
 
-    #dests=courses.objects.all()
-    #return render(request,'index.html',{'dests':dests})
     return render(request,'index.html')
 
+#return about page
+def about(request):
+
+
+    return render(request,'about.html')
+
+#return content page
+def content(request):
+
+
+    return render(request,'content.html')
+
+
+#return contact page
+def contact(request):
+
+
+    return render(request,'contact.html')
